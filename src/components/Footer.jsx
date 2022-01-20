ae2730585a185844a6906b07959384229ed7da01
Ervan Arianto <ervanari88@gmail.com>
2022-01-21 00:11:31 +0700

    fixx eror

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 // import { Link,NavLink } from 'react-router-dom';
 import {NavDropdown, Nav, Navbar} from 'react-bootstrap';
 class Footer extends Component {
-    render() {
-      
-    }
+    
 }
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,7 @@ import React, { Component } from 'react';
 // import { Link,NavLink } from 'react-router-dom';
 import {NavDropdown, Nav, Navbar} from 'react-bootstrap';
 class Header extends Component {
-    render() {
-      // return (
-        
-      
-      // );
-    }
+   
 }
 
 export default Header;
